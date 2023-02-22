3b4e262a972365774d6c6a45e676c3d03b33ae65
Jana <janajankina@rambler.ru>
2023-02-23 02:24:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+    public static void task1 () {
+        System.out.println("Задача1");}
+    public static void task2 () {
+        System.out.println("Задача2");}
+
+    public static void task3 () {
+        System.out.println("Задача3");}
+
+    public static void task4 () {
+        System.out.println("Задача4"); }
+
+    public static void task5 () {
+        System.out.println("Задача5");}
+
+    public static void task6 () {
+        System.out.println("Задача6");}
+
+    public static void task7 () {
+        System.out.println("Задача7");}
+
+    public static void task8 () {
+        System.out.println("Задача8");}
+
+
+
+}
